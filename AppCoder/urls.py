5ed23d14e3126fd21e0ba7d4d131b9514dd208ad
Aldana Serial <serialaldana@gmail.com>
2023-03-15 17:57:24 -0300

    Adicionando cambios en url

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
+from django.contrib import admin
 from AppCoder.views import cursos, crear_curso, estudiantes, profesores
 
 urlpatterns = [
+        path('admin/', admin.site.urls),
         path('cursos', cursos, name = "AppCoderCursos"),
         path('curso/<nombre>/<camada>', crear_curso, name = "AppCoderCurso"),
         path('estudiantes', estudiantes, name = "AppCoderEstudiantes"),
--- ProyectoCoder3675/urls.py
+++ ProyectoCoder3675/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib import admin
 from AppCoder.views import cursos, crear_curso, estudiantes, profesores
 
 urlpatterns = [
+        path('admin/', admin.site.urls),
         path('cursos', cursos, name = "AppCoderCursos"),
         path('curso/<nombre>/<camada>', crear_curso, name = "AppCoderCurso"),
         path('estudiantes', estudiantes, name = "AppCoderEstudiantes"),
