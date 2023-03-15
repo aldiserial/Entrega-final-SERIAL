34bcb2d0a3e78f0bbed9667c817c7a349af9aa0f
Aldana Serial <serialaldana@gmail.com>
2023-03-15 17:50:20 -0300

    haciendo modificaciones

---
+++ ProyectoCoder3675/urls.py
@@ -0,0 +1,24 @@
+"""ProyectoCoder3675 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from AppCoder.views import cursos, crear_curso, estudiantes, profesores
+
+urlpatterns = [
+        path('cursos', cursos, name = "AppCoderCursos"),
+        path('curso/<nombre>/<camada>', crear_curso, name = "AppCoderCurso"),
+        path('estudiantes', estudiantes, name = "AppCoderEstudiantes"),
+        path('profesores', profesores, name = "AppCoderProfesores"),
+]
