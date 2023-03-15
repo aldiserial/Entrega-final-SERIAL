867658ebb32cf211adb10f1f9f9773b5368999d1
Aldana Serial <serialaldana@gmail.com>
2023-03-15 18:50:58 -0300

    agregando datos al panel admin

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,3 +4,20 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
+
+    def __str__(self):
+        return f'Curso: {self.nombre}, Camada: {self.camada}'
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'Profesor: {self.nombre} {self.apellido}'
