c7c376164f460b814a78d5371f198b19949cf2a9
Aldana Serial <serialaldana@gmail.com>
2023-03-19 22:34:35 -0300

    ultima modificacion

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,10 +1,12 @@
 
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
-from AppCoder.views import cursos, crear_curso, estudiantes, profesores
+from AppCoder.views import *
 
 urlpatterns = [
+        path('', include('admin_soft.urls')),
         path('admin/', admin.site.urls),
+        path('buscar_curso', busqueda_curso, name="AppCoderBuscarCurso"),
         path('cursos', cursos, name = "AppCoderCursos"),
         path('curso/<nombre>/<camada>', crear_curso, name = "AppCoderCurso"),
         path('estudiantes', estudiantes, name = "AppCoderEstudiantes"),
--- ProyectoCoder3675/urls.py
+++ ProyectoCoder3675/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
-from AppCoder.views import cursos, crear_curso, estudiantes, profesores
+from AppCoder.views import *
 
 urlpatterns = [
+        path('', include('admin_soft.urls')),
         path('admin/', admin.site.urls),
         path('cursos', cursos, name = "AppCoderCursos"),
+        path('buscar_curso', busqueda_curso, name="AppCoderBuscarCurso"),
         path('curso/<nombre>/<camada>', crear_curso, name = "AppCoderCurso"),
         path('estudiantes', estudiantes, name = "AppCoderEstudiantes"),
         path('profesores', profesores, name = "AppCoderProfesores"),
